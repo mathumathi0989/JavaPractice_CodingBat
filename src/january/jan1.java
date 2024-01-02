b4db441396b7553979c69f0ec95438637d2c8723
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-01-02 12:42:26 -0500

    Add existing project files prior to the push to GitHub.

--- src/january/jan1.java
+++ src/january/jan1.java
@@ -4,26 +4,10 @@ public class jan1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//Given a string, return a string where for every char in the original, there are two chars.
-		
-		//doubleChar("The") → "TThhee"
-		
-		
-		
+
+		//github
 	}
 
-	
-public static String doubleChar(String str) {
 
-String s1 = "";
-
-for(int i=0; i<str.length(); i++){
-  
-  s1 = s1 + str.charAt(i)+str.charAt(i);
-}
-return s1;
-
-
-}
 	
 }
