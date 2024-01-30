c6dc61ea688ac0b68b6dcbbd9ba4023b070fae1f
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-01-29 21:06:30 -0500

    recursion

---
+++ src/january/jan29.java
@@ -0,0 +1,27 @@
+package january;
+
+public class jan29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+		
+		//count7(717) → 2
+		
+		System.out.println(count7(717));
+	}
+
+	public static int count7(int n) {
+ if (n < 1) return 0;
+
+  if (n % 10 == 7) 
+	  return 1 + count7(n/10);
+
+  else 
+	  return count7(n/10);
+
+  
+}
+
+	
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module JanCodingChallenge {
+	requires java.base;
 }
