4541f09dbe58b476c35fb44ef08466b15e728825
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-02-21 17:24:21 -0500

    interview practice

---
+++ src/February/feb21.java
@@ -0,0 +1,18 @@
+package February;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class feb21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int[] arr = new int[] {2,34,22323,23,1,245,55};
+		Arrays.sort(arr);
+		System.out.println(arr[0]);
+		System.out.println(arr[arr.length -1]);
+	}
+
+}
---
+++ src/February/feb8.java
@@ -0,0 +1,10 @@
+package February;
+
+public class feb8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
